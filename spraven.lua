360430d1d9c4834aaab43108633a420fb03b64af
Javier Orfo <124217829+javio7@users.noreply.github.com>
2023-02-07 15:57:35 -0300

    Create spraven.lua

---
+++ spraven.lua
@@ -0,0 +1,13 @@
+-- #######################################################
+-- # Maintainer:  Javier Orfo                            #
+-- # URL:         https://github.com/javio7/nvim-spraven #
+-- #######################################################
+
+if vim.g.spraven then
+  return
+end
+
+vim.g.spraven = 1
+
+-- vim.api.nvim_create_user_command('Command', 'lua require("spraven").some_function()', {})
+-- vim.api.nvim_set_keymap('n', '<Plug>Plug', '<cmd>lua require("spraven.core").some_function()<CR>', {})
