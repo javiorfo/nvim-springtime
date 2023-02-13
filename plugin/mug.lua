f53f8902d79f7589c2b12ec8573504c5e9c8a868
Mr. Charkuils <charkuils@proton.me>
2023-02-13 19:51:07 -0300

    Update and rename xpresso.lua to mug.lua

---
+++ plugin/mug.lua
@@ -0,0 +1,13 @@
+-- ######################################################
+-- # Maintainer:  Mr. Charkuils                         #
+-- # URL:         https://github.com/charkuils/nvim-mug #
+-- ######################################################
+
+if vim.g.mug then
+  return
+end
+
+vim.g.mug = 1
+
+-- vim.api.nvim_create_user_command('Command', 'lua require("mug").some_function()', {})
+-- vim.api.nvim_set_keymap('n', '<Plug>Plug', '<cmd>lua require("mug.core").some_function()<CR>', {})
