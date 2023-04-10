e50e4d84549865f31cbc354be742ae58dbf8a372
Mr. Charkuils <javierorfo@protonmail.com>
2023-04-10 11:55:48 -0300

    Update mug.lua

--- plugin/mug.lua
+++ plugin/mug.lua
@@ -1,6 +1,6 @@
 -- ######################################################
--- # Maintainer:  Javier Orfo                           #
--- # URL:         https://github.com/whiskoder/nvim-mug #
+-- # Maintainer:  Mr. Charkuils                         #
+-- # URL:         https://github.com/charkuils/nvim-mug #
 -- ######################################################
 
 if vim.g.mug then
