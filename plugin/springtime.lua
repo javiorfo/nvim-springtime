5e83c702bd9b89a60dcd59e08bdb8fd048253b9f
Mr. Charkuils <javierorfo@protonmail.com>
2023-04-22 09:21:52 -0300

    Update and rename mug.lua to springtime.lua

---
+++ plugin/springtime.lua
@@ -0,0 +1,8 @@
+if vim.g.springtime then
+  return
+end
+
+vim.g.springtime = 1
+
+-- vim.api.nvim_create_user_command('Command', 'lua require("springtime").some_function()', {})
+-- vim.api.nvim_set_keymap('n', '<Plug>Plug', '<cmd>lua require("springtime.core").some_function()<CR>', {})
