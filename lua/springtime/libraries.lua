a8fe41328d773262bd98ae82b87f746ddb3a0e7b
Javier Orfo <javierorfo@prontonmail.com>
2024-02-11 17:29:15 -0300

    nvim-cmp added for libraries

---
+++ lua/springtime/libraries.lua
@@ -0,0 +1,13 @@
+return {
+    { label = "Spring for Apache ActiveMQ",         insertText = "activemq " },
+    { label = "Spring Boot Actuator",               insertText = "actuator " },
+    { label = "Spring for RabbitMQ",                insertText = "amqp " },
+    { label = "Spring for Apache ActiveMQ Artemis", insertText = "artemis " },
+    { label = "Azure Active Directory",             insertText = "azure-active-directory " },
+    { label = "Azure Cosmos DB",                    insertText = "azure-cosmos-db " },
+    { label = "Azure Storage",                      insertText = "azure-storage " },
+    { label = "Azure Support",                      insertText = "azure-support " },
+    { label = "Spring Batch",                       insertText = "batch " },
+    { label = "Spring Cache Abstraction",           insertText = "cache " },
+    { label = "Apache Camel",                       insertText = "camel " },
+}
--- lua/springtime/ui.lua
+++ lua/springtime/ui.lua
@@ -1,6 +1,24 @@
 local popcorn = require'popcorn'
 local core = require'springtime.core'
+local libraries = require'springtime.libraries'
+local cmp = require'cmp'
 local M = {}
+
+local source = {}
+
+source.new = function()
+    return setmetatable({}, {__index = source})
+end
+
+source.complete = function(self, params, callback)
+    if vim.fn.line('.') >= 36 then
+        callback { items = libraries }
+    else
+        callback { items = {} }
+    end
+end
+
+cmp.register_source('spring_boot_libraries', source.new())
 
 function M.open()
     local opts = {
@@ -20,9 +38,10 @@ function M.open()
               end
             })
 
-            local library_list_a = function()
+--[[             local library_list_a = function()
+                vim.cmd("setlocal completeopt+=noinsert")
                 vim.fn.complete(vim.fn.col('.'), { "activemq", "actuator", "amqp", "artemis", "azure-active-directory",
-                "azure-cosmos-db", "azure-keyvault", "azure-storage", "azure-support"})
+                "azure-cosmos-db", "azure-keyvault", "azure-storage", "azure-support", "batch", "cache", "camel", "cloud-bus", "cloud-config-client", "cloud-config-server", "cloud-contract-stub-runner", "cloud-contract-verifier", "cloud-eureka", "cloud-eureka-server", "cloud-feign", "cloud-function", "cloud-gateway", "cloud-gateway-reactive", "cloud-gcp", "cloud-gcp-pubsub", "cloud-gcp-storage", "cloud-loadbalancer", "cloud-resilience4j", "cloud-starter", "cloud-starter-consul-config"})
             end
 
             local library_list_c = function()
@@ -30,8 +49,12 @@ function M.open()
                 return " "
             end
 
-            vim.keymap.set('i', 'a', library_list_a, { noremap = true, silent = true, buffer = 0 })
-            vim.keymap.set('i', 'c', library_list_c, { noremap = true, silent = true, buffer = 0 })
+            vim.keymap.set('i', '<Tab>', library_list_a, { noremap = true, silent = true, buffer = 0 }) ]]
+--             vim.keymap.set('i', 'c', library_list_c, { noremap = true, silent = true, buffer = 0 })
+
+            cmp.setup.buffer {
+                sources = { { name = 'spring_boot_libraries' } }
+            }
 
 --[[             vim.api.nvim_create_autocmd({"InsertEnter"}, {
               pattern = { "<buffer>" },
